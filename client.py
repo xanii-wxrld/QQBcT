e300ae368f34fc072c247a91205cd4293a662f16
Xanii <gucisosok@gmail.com>
2023-07-28 16:51:11 +0300

    1.5

--- client.py
+++ client.py
@@ -1,11 +1,75 @@
-<<<<<<< HEAD
 import socket
 import pygame
 import  math
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
+
+name = ""
+color = ""
+
+def login():
+    global name
+    name = row.get()
+    if name and color:
+        root.destroy()
+        root.quit()
+    else:
+        tk.messagebox.showerror("Ошибка", "Ты не выбрал цвет или не ввел имя :/")
+
+def scroll(event):
+    global color
+    color = combo.get()
+    style.configure("TCombobox", fieldbackground=color, background="white")
+
+def find(vector: str):
+    first = None
+    for num, sign in enumerate(vector):
+        if sign == "<":
+            first = num
+        if sign == ">" and first is not None:
+            second = num
+            result = map(float, vector[first + 1:second].split(","))
+            return result
+    return ""
+
+def draw_bct(data: list[str]):
+    for num, bact in enumerate(data):
+        data = bact.split(" ")
+        x = CC[0] + int(data[0])
+        y = CC[1] + int(data[1])
+        size = int(data[2])
+        color = data[3]
+        pygame.draw.circle(screen, color, (x, y), size)
+
+
+root = tk.Tk()
+root.title("Лог")
+root.geometry("300x200")
+style = ttk.Style()
+style.theme_use("clam")
+name_label = tk.Label(root, text="Введите свой никнейм: ")
+name_label.pack()
+row = tk.Entry(root, width=30, justify="center")
+row.pack()
+color_label = tk.Label(root, text="Выбор цвета: ")
+color_label.pack()
+colors = ['Maroon', 'DarkRed', 'FireBrick', 'Red', 'Salmon', 'Tomato', 'Coral', 'OrangeRed', 'Chocolate', 'SandyBrown',
+         'DarkOrange', 'Orange', 'DarkGoldenrod', 'Goldenrod', 'Gold', 'Olive', 'Yellow', 'YellowGreen', 'GreenYellow',
+         'Chartreuse', 'LawnGreen', 'Green', 'Lime', 'Lime Green', 'SpringGreen', 'MediumSpringGreen', 'Turquoise',
+         'LightSeaGreen', 'MediumTurquoise', 'Teal', 'DarkCyan', 'Aqua', 'Cyan', 'Dark Turquoise', 'DeepSkyBlue',
+         'DodgerBlue', 'RoyalBlue', 'Navy', 'DarkBlue', 'MediumBlue.']
+combo = ttk.Combobox(root, values=colors, textvariable=color)
+combo.bind("<<ComboboxSelected>>", scroll)
+combo.pack()
+name_btn = tk.Button(root, text="Зайти в игру", command=login)
+name_btn.pack()
+root.mainloop()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
 sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключаем пакетирование
 sock.connect(("localhost", 10000))
+sock.send(("color:<" + name + "," + color + ">").encode())
 
 pygame.init()
 
@@ -14,6 +78,7 @@ HEIGHT = 600
 CC = (WIDTH // 2, HEIGHT // 2)
 old = (0, 0)
 radius = 50
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("BCT")
@@ -31,28 +96,21 @@ while run:
         vector = vector[0] / lenv, vector[1] / lenv
         if lenv <= radius:
             vector = 0, 0
-    if vector != old:
-        old = vector
-        msg = f"<{vector[0]}, {vector[1]}>"
-        sock.send(msg.encode())
+        if vector != old:
+            old = vector
+            msg = f"<{vector[0]}, {vector[1]}>"
+            sock.send(msg.encode())
     screen.fill("gray")
-    pygame.draw.circle(screen, (0, 0, 0), CC, radius)
-    pygame.display.update()
-
+    pygame.draw.circle(screen, color, CC, radius)
     data = sock.recv(1024).decode()
-    print("получил: ", data)
+    data = find(data).split(",")
+    if data != ['']:
+        draw_bct(data)
+        pygame.display.update()
+
+    #data = sock.recv(1024).decode()
+    #print("получил: ", data)
+
+
 
 pygame.quit()
-
-=======
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Настраиваем сокет
-sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Отключаем пакетирование
-sock.connect(("localhost", 10000))
-
-while True:
-    sock.send("Приыет".encode())
-
-
->>>>>>> 05698eabbe4c22635f5f902214805d9995933345
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import pygame
 import psycopg2
 import random
 import faker
@@ -8,12 +9,52 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 
-
 engine = create_engine("postgresql+psycopg2://postgres:12345@localhost/zzzzz")
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
 s = Session()
+
+pygame.init()
+WIDHT_ROOM, HEIGHT_ROOM = 4000, 4000
+WIDHT_SERVER, HEIGHT_SERVER = 300, 300
+FPS = 100
+
+color = ['Maroon', 'DarkRed', 'FireBrick', 'Red', 'Salmon', 'Tomato', 'Coral', 'OrangeRed', 'Chocolate', 'SandyBrown',
+         'DarkOrange', 'Orange', 'DarkGoldenrod', 'Goldenrod', 'Gold', 'Olive', 'Yellow', 'YellowGreen', 'GreenYellow',
+         'Chartreuse', 'LawnGreen', 'Green', 'Lime', 'Lime Green', 'SpringGreen', 'MediumSpringGreen', 'Turquoise',
+         'LightSeaGreen', 'MediumTurquoise', 'Teal', 'DarkCyan', 'Aqua', 'Cyan', 'Dark Turquoise', 'DeepSkyBlue',
+         'DodgerBlue', 'RoyalBlue', 'Navy', 'DarkBlue', 'MediumBlue.']
+
+screen = pygame.display.set_mode((WIDHT_SERVER, HEIGHT_SERVER))
+pygame.display.set_caption("С")
+clock = pygame.time.Clock()
+
+
+us = {}
+
+
+def find(vector: str):
+    first = None
+    for num, sign in enumerate(vector):
+        if sign == "<":
+            first = num
+        if sign == ">" and first is not None:
+            second = num
+            result = map(float, vector[first + 1:second].split(","))
+            return result
+    return ""
+
+def find_color(info: str):
+    first = None
+    for num, sign in enumerate(info):
+        if sign == "<":
+            first = num
+        if sign == ">" and first is not None:
+            second = num
+            result = info[first + 1:second].split(",")
+            return result
+    return ""
 
 class Players(Base):
     __tablename__ = 'players'
@@ -24,13 +65,87 @@ class Players(Base):
     y = Column(Integer, default=500)
     size = Column(Integer, default=50)
     errors = Column(Integer, default=0)
-    abs_speed = Column(Integer, default=1)
-    speed_x = Column(Integer, default=0)
-    speed_y = Column(Integer, default=0)
+    abs_speed = Column(Integer, default=2)
+    speed_x = Column(Integer, default=2)
+    speed_y = Column(Integer, default=2)
+    color = Column(String(250), default="red")
+    w_vision = Column(Integer, default=800)
+    h_vision = Column(Integer, default=600)
 
     def __init__(self, name, address):
         self.name = name
         self.address = address
+
+
+class LocalPlayer:
+    def __init__(self, id, name, sock, addr):
+        self.id = id
+        self.db: Players = s.get(Players, self.id)
+        self.sock = sock
+        self.name = name
+        self.address = addr
+        self.x = 500
+        self.y = 500
+        self.size = 50
+        self.errors = 0
+        self.abs_speed = 1
+        self.speed_x = 0
+        self.speed_y = 0
+        self.color = "red"
+        self.w_vision = 800
+        self.h_vision = 600
+
+    def update(self):
+        self.x += self.speed_x
+        self.y += self.speed_y
+
+    def change_speed(self, vector):
+        vector = find(vector)
+        if vector[0] == 0 and vector[1] == 0:
+            self.speed_x = self.speed_y = 0
+
+        else:
+            vector = vector[0] * self.abs_speed, vector[1] * self.abs_speed
+            self.speed_x = vector[0]
+            self.speed_y = vector[1]
+
+    def sync(self):
+        self.db.size = self.size
+        self.db.abs_speed = self.abs_speed
+        self.db.speed_x = self.speed_x
+        self.db.speed_y = self.speed_y
+        self.db.errors = self.errors
+        self.db.x = self.x
+        self.db.y = self.y
+        self.db.color = self.color
+        self.db.w_vision = self.w_vision
+        self.db.h_vision = self.h_vision
+        s.merge(self.db)
+        s.commit()
+
+    def load(self):
+        self.size = self.db.size
+        self.abs_speed = self.db.abs_speed
+        self.speed_x = self.db.speed_x
+        self.speed_y = self.db.speed_y
+        self.errors = self.db.errors
+        self.x = self.db.x
+        self.y = self.db.y
+        self.color = self.db.color
+        self.w_vision = self.db.w_vision
+        self.h_vision = self.db.h_vision
+        return self
+
+
+
+
+
+
+
+
+
+
+Base.metadata.create_all(engine)
 
 main_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
@@ -40,16 +155,82 @@ main_socket.setblocking(False)
 main_socket.listen(5)
 print("Сокет создался")
 
-us = []
 
-while True:
+server_works = True
+while server_works:
+    clock.tick(FPS)
     try:
         new_socket, addr = main_socket.accept()
         print("Подключился", addr)
         new_socket.setblocking(False)
-        us.append(new_socket)
+        login = new_socket.recv(1024).decode()
+        # us.append(new_socket)
+        player = us("x", addr)
+        if login.startswith("color"):
+            data = find_color(login[6:])
+            player.name, player.color = data
+        s.merge(player)
+        s.commit()
+
+        addr = f"({addr[0]}, {addr[1]})"
+        data = s.query(Players).filter(Players.address == addr)
+        for us in data:
+            player = LocalPlayer(us.id, "x", new_socket, addr).load()
+            us[us.id] = player
     except BlockingIOError:
         pass
+    for id in list(us):
+        if us[id].sock is not None:
+            try:
+                data = us[id].sock.recv(1024).decode()
+                print("Получил", data)
+                us[id].change_speed(data)
+            except:
+                pass
+        else:
+            pass
+
+    visible_bacteries = {}
+    for id in list(us):
+        visible_bacteries[id] = []
+    pairs = list(us.items())
+    for i in range(0, len(pairs)):
+        for j in range(i + 1, len(pairs)):
+            hero_1: Players = pairs[i][1]
+            hero_2: Players = pairs[j][1]
+            dist_x = hero_2.x - hero_1.x
+            dist_y = hero_2.y - hero_1.y
+            if abs(dist_x) <= hero_1.w_vision // 2 + hero_2.size and abs(dist_y) <= hero_1.h_vision // 2 + hero_2.size:
+                x_ = str(round(dist_x))
+                y_ = str(round(dist_y))
+                size_ = str(round(hero_2.size))
+                color_ = hero_2.color
+                data = x_ + " " + y_ + " " + size_ + " " + color_
+                visible_bacteries[hero_1.id].append(data)
+
+            if abs(dist_x) <= hero_2.w_vision // 2 + hero_1.size and abs(dist_y) <= hero_2.h_vision // 2 + hero_1.size:
+                x_ = str(round(-dist_x))
+                y_ = str(round(-dist_y))
+                size_ = str(round(hero_1.size))
+                color_ = hero_1.color
+
+                data = x_ + " " + y_ + " " + size_ + " " + color_
+                visible_bacteries[hero_2.id].append(data)
+    for id in list(us):
+        visible_bacteries[id] = "<" + ",".join(visible_bacteries[id]) + ">"
+
+    for id in list(us):
+        try:
+            us[id].sock.send(visible_bacteries[id].encode())
+        except:
+            us[id].sock.close()
+            del us[id]
+            s.query(us).filter(us.id == id).delete()
+            s.commit()
+            print("Сокет закрыт")
+
+
+
 
     for sock in us:
         try:
@@ -59,8 +240,27 @@ while True:
             us.remove(sock)
             sock.close()
             print("Сокет закрыт")
-    time.sleep(1)
 
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            server_works = False
+
+    screen.fill("black")
+    for id in us:
+        player = us[id]
+        x = player.x * WIDHT_SERVER // WIDHT_ROOM
+        y = player.y * HEIGHT_SERVER // HEIGHT_ROOM
+        size = player.size * WIDHT_SERVER // WIDHT_ROOM
+        pygame.draw.circle(screen, player.color, (x, y), size)
+        for id in list(us):
+            player = us[id]
+            us[id].update()
+        pygame.display.update()
+
+pygame.quit()
+main_socket.close()
+s.query(Players).delete()
+s.commit()
 """import socket
 import time
 
@@ -89,4 +289,3 @@ while True:
             print("Получил", data)
         except:
             pass"""
-
